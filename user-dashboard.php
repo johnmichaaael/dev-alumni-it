c89553e76d8d07593484d10bbfd5ff57bc44b6d1
johnmichaaael <nvrmnd1991@gmail.com>
2024-08-06 16:12:46 +0800

    Modified user-dashboard.php

--- user-dashboard.php
+++ user-dashboard.php
@@ -34,7 +34,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="alumni-list.php">Alumni List</a>
+            <a class="nav-link active" aria-current="page" href="user-update.php">Update Info</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
